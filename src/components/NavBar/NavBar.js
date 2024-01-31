23333c760cd1cb1a8c7eac58a1a979697efc3d88
justjo3l <comedyandcomputers@gmail.com>
2024-01-31 15:57:59 +0400

    Add responsiveness to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,37 +2,52 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 import goldsteinLogo from '../../assets/goldstein-logo.png';
+import { useState } from 'react';
 
 function NavBar() {
-  return (
-    <nav id='navbar'>
-        <ul id='navbar-container'>
-            <li id='navbar-logo-container'>
-                <img id='navbar-logo' src={goldsteinLogo} alt='Goldstein Logo' />
-            </li>
-            <div id='navbar-links'>
-                <li>
-                    <Link to="/" className='link'>Home</Link>
+
+    const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    window.onresize = () => { setCurrentWidth(window.innerWidth) }
+
+    const onMenuClick = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    return (
+        <nav id='navbar'>
+            <ul id='navbar-container'>
+                <li id='navbar-logo-container'>
+                    <img id='navbar-logo' src={goldsteinLogo} alt='Goldstein Logo' />
+                    {currentWidth <= 800 &&
+                    <div onClick={onMenuClick} id='navbar-menu'>
+                        Menu
+                    </div>}
                 </li>
-                <li>
-                    <Link to="/about" className='link'>About</Link>
-                </li>
-                <li>
-                    <Link to="/dino" className='link'>Dino</Link>
-                </li>
-                <li>
-                    <Link to="/shop" className='link'>Shop</Link>
-                </li>
-                <li>
-                    <Link to="/house-committee" className='link'>House Committee</Link>
-                </li>
-                <li>
-                    <Link to="/tkc" className='link'>TKC</Link>
-                </li>
-            </div>
-        </ul>
-    </nav>
-  );
+                <div id='navbar-links' style={{display: currentWidth <= 800 && !menuOpen && "none"}}>
+                    <li>
+                        <Link to="/" className='link'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className='link'>About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dino" className='link'>Dino</Link>
+                    </li>
+                    <li>
+                        <Link to="/shop" className='link'>Shop</Link>
+                    </li>
+                    <li>
+                        <Link to="/house-committee" className='link'>House Committee</Link>
+                    </li>
+                    <li>
+                        <Link to="/tkc" className='link'>TKC</Link>
+                    </li>
+                </div>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar;
