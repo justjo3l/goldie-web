2b3e2b41bb0cc0b561ac321861e60682a0a521a1
justjo3l <comedyandcomputers@gmail.com>
2024-02-01 23:35:16 +0400

    Add required styling for highlight

--- src/assets/dummyData.js
+++ src/assets/dummyData.js
@@ -1,6 +1,6 @@
 const highlightData = [
 	{
-		title: "Javascript of Python?",
+		title: "Javascript or Python?",
 		description: "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 	},
 	{
--- src/components/HighlightCard/HighlightCard.js
+++ src/components/HighlightCard/HighlightCard.js
@@ -4,6 +4,8 @@ import "./HighlightCard.css";
 
 import propTypes from "prop-types";
 
+import harsh from "../../assets/harsh.jpeg";
+
 HighlightCard.propTypes = {
 	index: propTypes.number.isRequired,
 	highlight: propTypes.object.isRequired,
@@ -15,8 +17,13 @@ function HighlightCard(props) {
 
 	return (
 		<div key={index} className='highlight-card' id={`highlight-card-${index}`}>
-			<h2>{highlight.title}</h2>
-			<p>{highlight.description}</p>
+			<div className="highlight-card-content">
+				<img className='highlight-img' src={harsh} alt='Harsh' />
+				<div className='highlight-text-content'>
+					<h2 className='highlight-title'>{highlight.title}</h2>
+					<p className='highlight-desc'>{highlight.description}</p>
+				</div>
+			</div>
 		</div>);
 }
 
