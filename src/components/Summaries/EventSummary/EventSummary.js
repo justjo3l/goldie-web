d929ca0fdecc4a02384446bbb515c0e14dab4e07
justjo3l <comedyandcomputers@gmail.com>
2024-02-02 18:04:35 +0400

    Add shop summary content

--- src/components/Summaries/EventSummary/EventSummary.js
+++ src/components/Summaries/EventSummary/EventSummary.js
@@ -6,7 +6,7 @@ import "../Summary.css";
 function EventSummary() {
 	return (
 		<article className="summary">
-			<div className="summary-content">
+			<div className="summary-content" id='event-summary-content'>
 				<h4 id='event-summary-title'>Upcoming Event</h4>
 				<h2 id='event-summary-value'>Commencement Dinner</h2>
 			</div>
--- src/components/Summaries/ShopSummary/ShopSummary.js
+++ src/components/Summaries/ShopSummary/ShopSummary.js
@@ -6,8 +6,26 @@ import "../Summary.css";
 function ShopSummary() {
 	return (
 		<article className="summary">
-			<div className="summary-content">
-				SHOP
+			<div className="summary-content" id="shop-summary-content">
+				<h3 id='shop-summary-title'>Shop</h3>
+				<h4 id='shop-summary-reshtock-status'>Reshtocked</h4>
+				<section id='shop-summary-fields-container'>
+					<ul id='shop-summary-fields'>
+						<li className="shop-summary-field">
+							<label id='shlut-on-duty-label'>Shlut on Duty</label>
+							<p id='shlut-on-duty-value'>Alannah Scalzo</p>
+						</li>
+						<li className="shop-summary-field">
+							<label id='shopen-label'>Shopen</label>
+							<p id='shopen-value'>08:00 PM</p>
+						</li>
+						<li className="shop-summary-field">
+							<label id='shclose-label'>Shclose</label>
+							<p id='shclose-value'>11:00 PM</p>
+						</li>
+					</ul>
+				</section>
+				<h2 id='shop-summary-shopen-status'>Shclosed</h2>
 			</div>
 		</article>
 	);
