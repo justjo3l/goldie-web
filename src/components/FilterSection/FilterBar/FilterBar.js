59df1a2003ebc2e701b84f4d776660c01f1783a7
justjo3l <comedyandcomputers@gmail.com>
2024-02-02 14:51:44 +0400

    Add responsiveness to the filter bar

--- src/components/FilterSection/FilterBar/FilterBar.js
+++ src/components/FilterSection/FilterBar/FilterBar.js
@@ -6,36 +6,63 @@ import "./FilterBar.css";
 
 function FilterBar() {
 
+	const values = ["summary", "events", "announcements", "shop", "laundry"];
+
 	const { setSelected } = useContext(FilterContext);
 
-	const handleFilter = (e) => {
-		setSelected(e.target.value);
+	const updateSelected = (value) => {
 		const buttons = document.querySelectorAll(".filter-button");
 		buttons.forEach(button => {
 			button.classList.remove("selected");
 		});
-		e.target.classList.add("selected");
+		const selectedButton = document.querySelector(`[value=${value}]`);
+		selectedButton.classList.add("selected");
+	};
+
+	const handleScroll = (e) => {
+		let selectedIndex = 0;
+		const scrollPercentage = e.target.scrollLeft / e.target.scrollWidth;
+		if (scrollPercentage >= 0.1) {
+			selectedIndex = Math.ceil(scrollPercentage * (values.length - 1));
+		}
+		setSelected(values[selectedIndex]);
+		updateSelected(values[selectedIndex]);
+	};
+
+	const handleFilter = (e) => {
+		setSelected(e.target.value);
+		updateSelected(e.target.value);
 	};
 
 	return (
 		<nav id='filterbar'>
 			<ul id='filterbar-container'>
-				<div id='filterbar-options'>
-					<li>
-						<button className='filter-button selected' value='summary' onClick={handleFilter}>Summary</button>
-					</li>
-					<li>
-						<button className='filter-button' value='events' onClick={handleFilter}>Events</button>
-					</li>
-					<li>
-						<button className='filter-button' value='announcements' onClick={handleFilter}>Announcements</button>
-					</li>
-					<li>
-						<button className='filter-button' value='shop' onClick={handleFilter}>Shop</button>
-					</li>
-					<li>
-						<button className='filter-button' value='laundry' onClick={handleFilter}>Laundry</button>
-					</li>
+				<div id='filterbar-options' onScroll={handleScroll}>
+					<div>
+						<li>
+							<button className='filter-button selected' value='summary' onClick={handleFilter}>Summary</button>
+						</li>
+					</div>
+					<div>
+						<li>
+							<button className='filter-button' value='events' onClick={handleFilter}>Events</button>
+						</li>
+					</div>
+					<div>
+						<li>
+							<button className='filter-button' value='announcements' onClick={handleFilter}>Announcements</button>
+						</li>
+					</div>
+					<div>
+						<li>
+							<button className='filter-button' value='shop' onClick={handleFilter}>Shop</button>
+						</li>
+					</div>
+					<div>
+						<li>
+							<button className='filter-button' value='laundry' onClick={handleFilter}>Laundry</button>
+						</li>
+					</div>
 				</div>
 			</ul>
 		</nav>
