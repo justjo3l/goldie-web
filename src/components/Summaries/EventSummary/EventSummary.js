f1cf60a24b8e4f2fb155660c87813299d124c9b1
justjo3l <comedyandcomputers@gmail.com>
2024-02-02 19:13:47 +0400

    Add dummy APIs to give info for shop and event summaries

--- src/components/Summaries/EventSummary/EventSummary.js
+++ src/components/Summaries/EventSummary/EventSummary.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./EventSummary.css";
 import "../Summary.css";
 
+import { getEventSummary } from "../../../assets/dummyApis.js";
+
 function EventSummary() {
+
+	const [eventSummary, setEventSummary] = useState({
+		upcomingEvent: "",
+	});
+
+	useEffect(() => {
+		const newSummary = getEventSummary();
+		setEventSummary(newSummary);
+	}, []);
+
 	return (
 		<article className="summary">
 			<div className="summary-content" id='event-summary-content'>
 				<h4 id='event-summary-title'>Upcoming Event</h4>
-				<h2 id='event-summary-value'>Commencement Dinner</h2>
+				<h2 id='event-summary-value'>{eventSummary.upcomingEvent}</h2>
 			</div>
 		</article>
 	);
--- src/components/Summaries/ShopSummary/ShopSummary.js
+++ src/components/Summaries/ShopSummary/ShopSummary.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import getShopSummary from "../../../assets/dummyApis";
 
 import "./ShopSummary.css";
 import "../Summary.css";
 
 function ShopSummary() {
+
+	const [shopSummary, setShopSummary] = useState({
+		reshtockState: "",
+		shopenState: "",
+		shlutOnDuty: ""
+	});
+
+	const updateReshtockStateStyle = (shopSummary) => {
+		if (shopSummary.reshtockState === "reshtocked") {
+			document.getElementById("shop-summary-reshtock-status").classList.add("positive");
+		} else {
+			document.getElementById("shop-summary-reshtock-status").classList.add("negative");
+		}
+	};
+
+	const updateShopenStateStyle = (shopSummary) => {
+		if (shopSummary.shopenState === "shopen") {
+			document.getElementById("shop-summary-shopen-status").classList.add("positive");
+		} else {
+			document.getElementById("shop-summary-shopen-status").classList.add("negative");
+		}
+	};
+
+	useEffect(() => {
+		const newSummary = getShopSummary();
+		updateReshtockStateStyle(newSummary);
+		updateShopenStateStyle(newSummary);
+		setShopSummary(newSummary);
+	}, []);
+
 	return (
 		<article className="summary">
 			<div className="summary-content" id="shop-summary-content">
 				<h3 id='shop-summary-title'>Shop</h3>
-				<h4 id='shop-summary-reshtock-status'>Reshtocked</h4>
+				<h4 id='shop-summary-reshtock-status'>{shopSummary.reshtockState}</h4>
 				<section id='shop-summary-fields-container'>
 					<ul id='shop-summary-fields'>
 						<li className="shop-summary-field">
 							<label id='shlut-on-duty-label'>Shlut on Duty</label>
-							<p id='shlut-on-duty-value'>Alannah Scalzo</p>
+							<p id='shlut-on-duty-value'>{shopSummary.shlutOnDuty}</p>
 						</li>
 						<li className="shop-summary-field">
 							<label id='shopen-label'>Shopen</label>
@@ -25,7 +57,7 @@ function ShopSummary() {
 						</li>
 					</ul>
 				</section>
-				<h2 id='shop-summary-shopen-status'>Shclosed</h2>
+				<h2 id='shop-summary-shopen-status'>{shopSummary.shopenState}</h2>
 			</div>
 		</article>
 	);
